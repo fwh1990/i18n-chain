c0a10baa61719d9d5ac7b2a04b9a3351724fc21b
范文华 <531362022@qq.com>
2019-12-27 16:31:17 +0800

    feat: Support string literal

--- src/I18n.ts
+++ src/I18n.ts
@@ -12,20 +12,42 @@ interface I18nConfig<T> {
   loader?: (key: string) => Promise<any>;
 }
 
-export class I18n<T extends object> {
-  protected readonly defaultLanguage: Language<T>;
+type HasDefault<T> = {[key in keyof T]: T[key] extends undefined ? never : key}[keyof T];
+
+type NoDefault<T> = {[key in keyof T]: T[key] extends undefined ? key : never}[keyof T];
+
+type Locale<U extends object> = {
+  [key in keyof U]: U[key] extends Array<any>
+    ? Extract<U[key][number], object> extends infer R
+      ? (
+        params: NoDefault<R> extends never
+          ? HasDefault<R> extends never
+            ? never
+            : { [p in HasDefault<R>]?: string | number }
+          : HasDefault<R> extends never
+            ? { [p in NoDefault<R>]: string | number }
+            : { [p in NoDefault<R>]: string | number } & { [p in HasDefault<R>]?: string | number }
+        ) => Exclude<U[key][0], object>
+      : never
+    : U[key] extends object
+      ? Locale<U[key]>
+      : U[key]
+};
+
+export class I18n<U extends object, T = Locale<U>> {
+  protected readonly defaultLanguage: Language<U>;
   protected readonly loader: I18nConfig<T>['loader'];
-  protected languages: Partial<Record<string, Language<T>['values']>> = {};
+  protected languages: Partial<Record<string, Language<U>['values']>> = {};
   protected hashes: Partial<Record<string, string>> = {};
   protected currentLanguageName: string = '';
   // @ts-ignore
-  protected currentLanguage: T = {};
+  protected currentLanguage: U = {};
 
-  protected listeners: Function[] = [];
+  protected listeners: ((locale: string) => void)[] = [];
 
   protected caches: Record<string, ProxyConstructor> = {};
 
-  constructor(config: I18nConfig<T>) {
+  constructor(config: I18nConfig<U>) {
     this.defaultLanguage = config.defaultLanguage;
     this.loader = config.loader;
 
@@ -34,7 +56,7 @@ export class I18n<T extends object> {
       .use(config.defaultLanguage.key);
   }
 
-  public define(key: string, values: T): this {
+  public define(key: string, values: U): this {
     this.languages[key] = values;
 
     return this;
@@ -74,18 +96,33 @@ export class I18n<T extends object> {
     return this;
   }
 
-  public get data(): T {
-    // TODO: Cache proxy
-
+  public get chain(): T {
     // @ts-ignore
     return new Proxy(this, {
-      get: (target, property: string | number) => {
-        if (target.currentLanguage[property]) {
-          return this.proxy(target.currentLanguage, property, [], false);
+      get: (target, property) => {
+        if (!this.isValidProperty(property)) {
+          return undefined;
         }
 
-        if (target.defaultLanguage[property]) {
-          return this.proxy(target.defaultLanguage, property, [], true);
+        const properties: string[] = property.split('.');
+        const first = properties.shift()!;
+        let result: any;
+        let hasData: boolean = false;
+
+        if (target.currentLanguage[first]) {
+          hasData = true;
+          result = this.proxy(target.currentLanguage[first], [first], false);
+        } else if (target.defaultLanguage[first]) {
+          result = this.proxy(target.defaultLanguage[first], [first], true);
+          hasData = true;
+        }
+
+        if (hasData) {
+          for (const name of properties) {
+            result = result[name];
+          }
+
+          return result;
         }
 
         console.error(`I18n can not find property "${property}"`);
@@ -95,29 +132,62 @@ export class I18n<T extends object> {
     });
   }
 
-  protected proxy(parent: any, child: string | number, properties: Array<string | number>, useDefault: boolean) {
-    const data = parent[child];
-    const newProperties = properties.concat(child);
+  protected proxy(data: any, allProperties: string[], useDefault: boolean) {
+    if (Array.isArray(data)) {
+      return (params: object): string => {
+        let message: string = data[0];
+        const defaultParams: object = data[1]; 
+        const newParams = Object.assign({}, defaultParams, params);
 
-    if (data === undefined) {
-      if (useDefault) {
-        return newProperties.join('.');
-      }
-
-      // Fallback to default language
-      let newData = this.defaultLanguage;
-
-      for (const property of properties) {
-        newData = newData[property];
-
-        if (newData === undefined) {
-          return newProperties.join('.');
+        for (const key of Object.keys(newParams)) {
+          if (typeof newParams[key] === 'string' || typeof newParams[key] === 'number') {
+            message = message.replace(new RegExp(`\{\{${key}\}\}`, 'gm'), newParams[key]);
+          }
         }
-      }
+
+        return message;
+      };
     } else if (typeof data === 'object') {
       return new Proxy(data, {
-        get: (target, property: string | number) => {
-          return this.proxy(target, property, newProperties, useDefault);
+        get: (target, property) => {
+          if (!this.isValidProperty(property)) {
+            return undefined;
+          }
+
+          const properties: string[] = property.split('.');
+          const first = properties.shift()!;
+          const newAllProperties = allProperties.concat(first);
+          let proxyData = target[first];
+
+          if (proxyData === undefined) {
+            if (useDefault) {
+              // TODO: May be throw Error
+              return newAllProperties.join('.');
+            }
+      
+            // Fallback to default language
+            proxyData = this.defaultLanguage;
+      
+            for (const name of allProperties) {
+              proxyData = proxyData[name];
+      
+              if (proxyData === undefined) {
+                // TODO: May be throw Error
+                return newAllProperties.join('.');
+              }
+            }
+
+            // Found key in default language
+            useDefault = true;
+          }
+          
+          let result = this.proxy(proxyData, newAllProperties, useDefault);
+
+          for (const name of properties) {
+            result = result[name];
+          }
+
+          return result;
         },
       });
     }
@@ -125,7 +195,11 @@ export class I18n<T extends object> {
     return data;
   }
 
-  protected listen(fn: Function): UnListen {
+  protected isValidProperty(property: string | number | symbol): property is string {
+    return property !== '$$typeof' && typeof property === 'string';
+  }
+
+  protected listen(fn: (locale: string) => void): UnListen {
     this.listeners.push(fn);
 
     return () => {
@@ -133,8 +207,8 @@ export class I18n<T extends object> {
     };
   }
 
-  protected publish(values: T): void {
+  protected publish(values: U): void {
     this.currentLanguage = values;
-    this.listeners.forEach((listener) => listener());
+    this.listeners.forEach((listener) => listener(this.currentLanguageName));
   }
 }
--- src/hooks.ts
+++ src/hooks.ts
@@ -1,17 +1,17 @@
 import { I18n } from './I18n';
 import { useState, useEffect } from 'react';
 
-export function useI18n<T extends object>(i18n: I18n<T>): T {
-  const [, setCount] = useState(0);
+export function useI18n<G extends object, T extends object>(i18n: I18n<G, T>): T {
+  const [data, setData] = useState(() => i18n.chain);
 
   useEffect(() => {
-     // @ts-ignore
+    // @ts-ignore
     const unListen = i18n.listen(() => {
-      setCount((prev) => prev + 1);
+      setData(i18n.chain);
     });
 
     return unListen;
-  }, []);
+  }, [i18n]);
 
-  return i18n.data;
+  return data;
 }
