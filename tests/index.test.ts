3bbd71459934b34cbd074c815a23d8183f39e755
范文华 <531362022@qq.com>
2020-11-03 18:57:13 +0800

    tests: Add cases

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -175,12 +175,17 @@ it ('can get literal', () => {
   expect(i18n.literal.profile.info1).to.equal('profile.info1');
   expect(i18n.chain.profile.info1).to.equal('Here is the profile');
   expect(i18n.literal.profile.info1).to.equal('profile.info1');
+  expect(i18n.t(i18n.literal.profile.info1)).to.equal('Here is the profile');
 
   expect(i18n.literal.defaultValue.info2.info3).to.equal('defaultValue.info2.info3');
   expect(i18n.chain.defaultValue.info2.info3).to.equal('Deeply default value');
+  expect(i18n.t(i18n.literal.defaultValue.info2.info3)).to.equal('Deeply default value');
+
   expect(i18n.literal.defaultValue.info2.info4).to.equal('defaultValue.info2.info4');
   expect(i18n.chain.defaultValue.info2.info4).to.equal('Deeply default value 4');
+  expect(i18n.t(i18n.literal.defaultValue.info2.info4)).to.equal('Deeply default value 4');
 
   expect(i18n.literal.name).to.equal('name');
   expect(i18n.chain.name).to.equal('English');
+  expect(i18n.t(i18n.literal.name)).to.equal('English');
 });
